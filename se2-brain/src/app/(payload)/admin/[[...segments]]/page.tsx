12bff6d6005d80e5448831b907227580c18f2680
se2code <se2@se2code.engineer>
2026-01-17 06:34:09 -0500

    chore: add debug logs to generateMetadata and use relative config import

--- se2-brain/src/app/(payload)/admin/[[...segments]]/page.tsx
+++ se2-brain/src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -1,7 +1,7 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 import type { Metadata } from 'next'
 
-import config from '@payload-config'
+import config from '../../../payload.config'
 import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
 import { importMap } from '../importMap'
 
@@ -14,8 +14,11 @@ type Args = {
     }>
 }
 
-export const generateMetadata = async ({ params, searchParams }: Args): Promise<Metadata> =>
-    generatePageMetadata({ config, params, searchParams })
+export const generateMetadata = async ({ params, searchParams }: Args): Promise<Metadata> => {
+    console.log('--- DEBUG METADATA ---')
+    console.log('Config loaded:', !!config)
+    return generatePageMetadata({ config, params, searchParams })
+}
 
 const Page = async ({ params, searchParams }: Args) => {
     console.log('--- DEBUG PAGE ---')
